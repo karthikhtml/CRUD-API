6199fdc0c69ea5a1c66405cb6a22d5fe492a1039
karthikhtml <106761441+karthikhtml@users.noreply.github.com>
2024-06-17 17:03:42 +0530

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const session = require('express-session')
-
 const router = require('./routes/route')
+const connectDB = require('./lib/db')
 
-require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(process.env.URI).then(() => {
-  console.log('Database connected successfully')
-})
+connectDB()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
